e35f804b7d63380d905e89817c2dc5d848074094
Salman Maarouf <salman.k.maarouf@gmail.com>
2025-07-08 21:15:57 +1000

    creatMany functions

--- script.ts
+++ script.ts
@@ -4,6 +4,21 @@ const prisma = new PrismaClient()
 
 async function main() {
   await prisma.user.deleteMany()
+  const users = await prisma.user.createMany({
+    data: [{
+      name: "Kyle",
+      email: "kyle@test.com",
+      age: 27,
+    }, {
+      name: "Sally",
+      email: "sally@test.com",
+      age: 32,
+    }
+  
+  ]
+  })
+
+  console.log(users)
 }
 
 main()
